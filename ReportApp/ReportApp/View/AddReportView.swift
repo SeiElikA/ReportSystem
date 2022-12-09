87255c70b47c7528fa26270e7272554214382651
YehJiaJun <a159663@hotmail.com>
2022-12-09 15:09:29 +0800

    fix

---
+++ ReportApp/ReportApp/View/AddReportView.swift
@@ -0,0 +1,20 @@
+//
+//  AddReportView.swift
+//  ReportApp
+//
+//  Created by 葉家均 on 2022/12/9.
+//
+
+import SwiftUI
+
+struct AddReportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddReportView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddReportView()
+    }
+}
